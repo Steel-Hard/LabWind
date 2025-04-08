f31a54970fcff2f140982ad969a934dd92d8a4dc
claudsaints <claudiodsantoss7@gmail.com>
2025-04-07 22:18:52 -0300

    backend: IUser (interface) and role enum, bcrypt hash initial config

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,11 +1,17 @@
 import { Request,Response } from "express";
 import userModel from "../models/user";
+import IUser from "../types/interfaces/IUser";
+import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken";
+
 
 class UserController{
     public async createUser(req:Request,res:Response){
-        const {name,email,password} = req.body;
+        const {name,email,password,role} = req.body as IUser;
+        const nPassword = await bcrypt.hash(password,8);
+
         try {
-            const data = await userModel.insertOne({name,email,password});            
+            const data = await userModel.insertOne({name,email,password:nPassword,role});            
             res.status(201).json({data});
         } catch (error) {
             console.log(error);
@@ -15,7 +21,18 @@ class UserController{
     public async readUser(req:Request,res:Response){
         const {email,password} = req.body;
         try {
-            const data = await userModel.find({email,password});            
+
+            const data = await userModel.findOne({email: email});
+
+            // if(data?.password) await bcrypt.compare(password,data.password)
+            //     .then((boolean) => {
+            //         boolean == true?  
+            //         :
+            //     })
+                
+            //     jwt.sign({}, ,{})
+            
+            
             res.status(302).json({data});
         } catch (error) {
             console.log(error);
