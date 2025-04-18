75ea7dce72227085f912716f7c32b72806bf24af
brn-lc <brenol_boff@hotmail.com>
2025-04-17 21:08:07 -0300

    Mudanças no cabeçalho e texto da landing page

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,19 +1,18 @@
 import { Link } from "react-router-dom";
 
-function Header(){
-    return(
-        <>
-        <div className="sidebar bg-transparent">
-            <h1>LabWind</h1>
-            <div className="flex text-black align-middle justify-center gap-3 text-4xl">
-
-            <Link to={"/login"}>
-                Login &raquo;
-            </Link>
-            </div>
+function Header() {
+  return (
+    <>
+      <div className="sidebar bg-transparent">
+        <h1 className="jomhuria-regular text-8xl text-shadow-lg/100">
+          LabWind
+        </h1>
+        <div className="flex text-black align-middle justify-center gap-3 text-4xl">
+          <Link to={"/login"}>Login &raquo;</Link>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
 export default Header;
--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,30 +1,37 @@
 import { Header } from "../components";
 function Landing() {
   return (
-        <>
-        <div className="flex-1">
-
+    <>
+      <div className="flex-1">
         <Header />
-      
+
         <div className="h-screen w-full">
-          <img className="w-full h-full z-0 invisible md:visible sm:invisible" src="./bg-rg.svg"/>
-          <img className="z-40 rounded-2xl  absolute w-120 h-120 top-10 md:left-1/6 sm:left-2/4" src="./furnas.png"></img>
-          <p className=" text-black absolute text-justify  h-120 w-65 top-45 right-1/6">
-          Solução tecnológica para monitoramento climático no Lago de Furnas, oferecendo gráficos interativos e alertas em tempo real, visando reduzir riscos para navegantes e comunidades locais.
+          <img
+            className="w-full h-full z-0 invisible md:visible sm:invisible"
+            src="./bg-rg.svg"
+          />
+          <img
+            className="z-40 rounded-2xl  absolute w-120 h-120 top-10 md:left-1/6 sm:left-2/4"
+            src="./furnas.png"
+          ></img>
+          <p className="roboto-medium sm:text-white md:text-black lg:text-black absolute text-justify text-4xl h-[300px] w-[500px] top-45 right-1/6">
+            Solução tecnológica para monitoramento climático no Lago de Furnas,
+            oferecendo gráficos interativos e alertas em tempo real, visando
+            reduzir riscos para navegantes e comunidades locais.
           </p>
 
-          <button onClick={() => { window.location.href = "/cadastro"}} className="button absolute w-60 h-20 bottom-30 right-1/6">
+          <button
+            onClick={() => {
+              window.location.href = "/cadastro";
+            }}
+            className="button absolute w-60 h-20 bottom-30 right-1/6"
+          >
             Cadastrar
           </button>
-
-         </div>       
         </div>
-
-
-
-
-      </>
-      )
+      </div>
+    </>
+  );
 }
 
-      export default Landing;
+export default Landing;
