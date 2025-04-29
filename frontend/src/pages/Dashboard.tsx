517a54eee1b32deaae8460914552918719f1d454
frevisto <vitor.zonzini@fatec.sp.gov.br>
2025-04-28 21:35:40 -0300

    Add useState para o Dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles.css'
 
 const Dashboard: React.FC = () => {
+  const [selectedStation, setSelectedStation] = useState('all');
+
   return (
     <div className="dashboard-container">
       {/* Agrupando para o hover funcionar */}
@@ -20,12 +22,38 @@ const Dashboard: React.FC = () => {
 
       {/* MainContent */}
       <div className="main-content">
+      <div className="station-select">
+        <label htmlFor="station">Selecionar Estação:</label>
+        <select
+          id="station"
+          value={selectedStation}
+          onChange={(e) => setSelectedStation(e.target.value)}
+        >
+          <option value="all">Todas as Estações</option>
+          <option value="station1">Estação 1</option>
+          <option value="station2">Estação 2</option>
+          <option value="station3">Estação 3</option>
+        </select>
+      </div>
         <div className="grid-container">
-          {Array.from({ length: 6 }).map((_, index) => (
-            <div key={index} className="card">
-              <p className="card-text">Card {index + 1}</p>
+        <div key={0} className="card">
+              <p className="card-text">Vento</p>
             </div>
-          ))}
+            <div key={1} className="card">
+              <p className="card-text">Temperatura</p>
+            </div>
+            <div key={2} className="card">
+              <p className="card-text">Pressão</p>
+            </div>
+            <div key={3} className="card">
+              <p className="card-text">Umidade</p>
+            </div>
+            <div key={4} className="card">
+              <p className="card-text">Radiação Solar</p>
+            </div>
+            <div key={5} className="card">
+              <p className="card-text">Ondas</p>
+            </div>
         </div>
       </div>
     </div>
