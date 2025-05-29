9edc351e0ac468d6ea99c74717a33fbe845f4c68
frevisto <vitor.zonzini@fatec.sp.gov.br>
2025-05-29 19:35:33 -0300

    rota /token

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,14 +3,16 @@ import userController from "./userRoute";
 import sensorDataRoutes from "./sensorDataRoute";
 import labwindDataController from "./labwindDataRoute";
 import barragemRoutes from "./barragemRoutes";
-
+import { authenticateToken } from "../middlewares/jwt";
 const routes = Router();
 
 routes.use("/users", userController);
 routes.use("/sensor-data", sensorDataRoutes);
 routes.use("/labwind", labwindDataController);
 routes.use("/barragem", barragemRoutes);
-
+routes.post("/token", authenticateToken, (req:Request,res:Response)=>{
+    res.status(200).send("Token válido")
+});
 
 routes.use((_req:Request, _res: Response) => {
     _res.status(404).json({message:"Resouce Not Found"});
