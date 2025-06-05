60f040b8f2c694f31d6aa34070016c3601e9f3a8
frevisto <vitor.zonzini@fatec.sp.gov.br>
2025-06-04 22:02:02 -0300

    add leaflet

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Header, Nav, WeatherAlerts, WeatherCard } from '../components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Header, Nav, WeatherAlerts, WeatherCard } from "../components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faWind,
   faTemperatureHigh,
@@ -8,19 +8,22 @@ import {
   faSun,
   faGaugeHigh,
   faDroplet,
-
-} from '@fortawesome/free-solid-svg-icons';
-import { useState, useEffect } from 'react';
-import BarragemService from '../services/BarragemService';
-import { ISensorData, IWeatherData } from '../types';
-import LabwindDataService from '../services/LabwindDataService';
-import "./styles/Dashboard.css"
-import OpenWeatherService from '../services/OpenWeather';
+} from "@fortawesome/free-solid-svg-icons";
+import { useState, useEffect } from "react";
+import BarragemService from "../services/BarragemService";
+import { ISensorData, IWeatherData } from "../types";
+import LabwindDataService from "../services/LabwindDataService";
+import "./styles/Dashboard.css";
+import OpenWeatherService from "../services/OpenWeather";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 const Dashboard: React.FC = () => {
   const [sensor, setSensor] = useState<ISensorData>();
   const [barragemData, setBarragemData] = useState<string | any>();
   const [previsao, setPrevisao] = useState<IWeatherData>();
+  const position: [number, number] = [-21.026146884566906, -46.13282320900263];
 
   useEffect(() => {
     const fetchSensorData = async () => {
@@ -48,12 +51,11 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     const fetchPrevisao = async () => {
-        const response = await OpenWeatherService.getPrevisao() ;
-        setPrevisao(response);
-      
-    }
-    fetchPrevisao()
-  }, [])
+      const response = await OpenWeatherService.getPrevisao();
+      setPrevisao(response);
+    };
+    fetchPrevisao();
+  }, []);
 
   return (
     <>
@@ -65,72 +67,103 @@ const Dashboard: React.FC = () => {
         <div className="grid-container grid grid-cols-3 gap-8 w-full max-w-[1800px]">
           <WeatherCard
             title="Temperatura"
-            value={sensor?.temp !== undefined ? sensor.temp : '--'}
+            value={sensor?.temp !== undefined ? sensor.temp : "--"}
             unit="°C"
-            icon={<FontAwesomeIcon size={'5x'} color='black' icon={faTemperatureHigh} />}
+            icon={
+              <FontAwesomeIcon
+                size={"5x"}
+                color="black"
+                icon={faTemperatureHigh}
+              />
+            }
             type="temperature"
           />
           <WeatherCard
             title="Umidade"
-            value={sensor?.hum !== undefined ? sensor.hum : '--'}
+            value={sensor?.hum !== undefined ? sensor.hum : "--"}
             unit="%"
-            icon={<FontAwesomeIcon size={'5x'} color='black' icon={faDroplet} />}
+            icon={
+              <FontAwesomeIcon size={"5x"} color="black" icon={faDroplet} />
+            }
             type="humidity"
           />
           <WeatherCard
             title="Pressão Atmosférica"
-            value={sensor?.bar !== undefined ? sensor.bar : '--'}
+            value={sensor?.bar !== undefined ? sensor.bar : "--"}
             unit="hPa"
-            icon={<FontAwesomeIcon size={'5x'} color='black' icon={faGaugeHigh} />}
+            icon={
+              <FontAwesomeIcon size={"5x"} color="black" icon={faGaugeHigh} />
+            }
             type="pressure"
           />
           <WeatherCard
             title="Radiação Solar"
-            value={sensor?.uv_level !== undefined ? sensor.uv_level : '--'}
+            value={sensor?.uv_level !== undefined ? sensor.uv_level : "--"}
             unit="W/m²"
-            icon={<FontAwesomeIcon size={'5x'} color='black' icon={faSun} />}
+            icon={<FontAwesomeIcon size={"5x"} color="black" icon={faSun} />}
             type="solarRadiation"
           />
           <WeatherCard
             title="Velocidade do Vento"
-            value={sensor?.wind_rt !== undefined ? sensor.wind_rt : '--'}
+            value={sensor?.wind_rt !== undefined ? sensor.wind_rt : "--"}
             unit="m/s"
-            icon={<FontAwesomeIcon size={'5x'} color='black' icon={faWind} />}
+            icon={<FontAwesomeIcon size={"5x"} color="black" icon={faWind} />}
             type="windSpeed"
           />
           <WeatherCard
             title="Direção do Vento"
-            value={sensor?.wind_dir_rt !== undefined ? sensor.wind_dir_rt : '--'}
+            value={
+              sensor?.wind_dir_rt !== undefined ? sensor.wind_dir_rt : "--"
+            }
             unit="°"
-            icon={<FontAwesomeIcon size={'5x'} color='black' icon={faWind} />}
+            icon={<FontAwesomeIcon size={"5x"} color="black" icon={faWind} />}
             type="windDirection"
           />
-          
           <WeatherCard
             title="Previsão do tempo"
-            value={previsao?.weather !== undefined ? previsao.weather[0].description: '--'}
-            icon={<FontAwesomeIcon size={'5x'} color='black' icon={faWind} />}
+            value={
+              previsao?.weather !== undefined
+                ? previsao.weather[0].description
+                : "--"
+            }
+            icon={<FontAwesomeIcon size={"5x"} color="black" icon={faWind} />}
             type="previsao"
             espValue={previsao?.weather[0].main}
           />
-
           {barragemData && (
             <WeatherCard
               title="Volume da Barragem"
               value={barragemData}
               unit="%"
-              icon={<FontAwesomeIcon size={'5x'} color='black' icon={faWater} />}
+              icon={
+                <FontAwesomeIcon size={"5x"} color="black" icon={faWater} />
+              }
               type="dam"
             />
           )}
-          <div className="weather-card bg-white rounded-lg shadow-md p-4 flex items-center justify-center min-h-[220px] min-w-[400px]">
-            <span className="text-gray-400">Card Vazio</span>
-          </div>
+<div className="weather-card bg-white rounded-lg shadow-md p-4 flex flex-col min-h-[220px] min-w-[400px]">
+  <div className="flex-grow w-full h-full">
+    <MapContainer
+      center={position}
+      zoom={8}
+      scrollWheelZoom={true}
+      style={{ height: "100%", width: "100%" }}
+    >
+      <TileLayer
+        attribution=""
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>Represa de Furnas</Popup>
+      </Marker>
+    </MapContainer>
+  </div>
+</div>
+
         </div>
       </div>
 
-
-      <WeatherAlerts/>
+      <WeatherAlerts />
     </>
   );
 };
